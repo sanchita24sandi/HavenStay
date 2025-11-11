9563b8b8c805e99b5acc6eb44829f7e96e64cfd6
BultiGorai <sanchitasandi987@gmial.com>
2025-11-11 15:15:05 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required:true,
+    },
+    description: String,
+    image:{
+       type: String,
+       default:"https://images.unsplash.com/photo-1761960084255-7b45bd632251?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1172",
+       set:(v)=>v===""
+       ?"https://images.unsplash.com/photo-1761960084255-7b45bd632251?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1172"
+       :v,},
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const listing = mongoose.model("listing", listingSchema);
+module.exports = listing;
